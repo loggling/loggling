97849e9f72cacacd95285077b3dae1949cf154fd
solgitae <dlwodud821@gmail.com>
2026-03-18 16:39:27 +0900

    feat: implement JsonMasker and FieldFilter processors

---
+++ pkg/processor/masker.go
@@ -0,0 +1,31 @@
+package processor
+
+import (
+	"bytes"
+
+	"github.com/loggling/loggling/pkg/model"
+)
+
+type JsonMasker struct {
+	TargetField []byte
+	Strategy    model.MaskStrategy
+}
+
+func (m *JsonMasker) Name() string {
+	return "JSON_MASKER"
+}
+
+func (m *JsonMasker) Process(payload *model.LogPayload) bool {
+	for _, idx := range payload.FieldIndices {
+		key := payload.Data[idx.KeyStart:idx.KeyEnd]
+
+		if bytes.Equal(key, m.TargetField) {
+			valueRange := payload.Data[idx.ValStart:idx.ValEnd]
+			m.Strategy.Mask(valueRange)
+
+			return true
+		}
+	}
+
+	return true
+}
