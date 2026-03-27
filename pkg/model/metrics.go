d09865035534c08f513afbd925f165051ebe93d0
solgitae <solgitae@outlook.com>
2026-03-28 07:41:29 +0900

    feat: add atomic metrics engine for real-time monitoring

---
+++ pkg/model/metrics.go
@@ -0,0 +1,30 @@
+package model
+
+import "sync/atomic"
+
+type Metrics struct {
+	ProcessedLines atomic.Uint64
+	ProcessedBytes atomic.Uint64
+	DroppedLines   atomic.Uint64
+	ErrorLines     atomic.Uint64
+	TotalLatency   atomic.Uint64
+}
+
+var GlobalMetrics = &Metrics{}
+
+func (m *Metrics) AddProcessedLine(size int) {
+	m.ProcessedLines.Add(1)
+	m.ProcessedBytes.Add((uint64(size)))
+}
+
+func (m *Metrics) AddDroppedLine() {
+	m.DroppedLines.Add(1)
+}
+
+func (m *Metrics) AddErrorLine() {
+	m.ErrorLines.Add(1)
+}
+
+func (m *Metrics) AddLatency(nanos int64) {
+	m.TotalLatency.Add(uint64(nanos))
+}
