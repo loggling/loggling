7264b0c96b1ee2a9dc7dde84d37fd29873364a00
solgitae <solgitae@outlook.com>
2026-03-27 10:42:37 +0900

    feat: add log deduplication processor

---
+++ pkg/processor/deduplication.go
@@ -0,0 +1,24 @@
+package processor
+
+import (
+	"bytes"
+
+	"github.com/loggling/loggling/pkg/model"
+)
+
+type DeduplicationProcessor struct {
+	lastLog []byte
+}
+
+func (d *DeduplicationProcessor) Name() string {
+	return "JSON_DEDUPLICATION"
+}
+
+func (d *DeduplicationProcessor) Process(payload *model.LogPayload) bool {
+	if bytes.Equal(d.lastLog, payload.Data) {
+		return false
+	}
+
+	d.lastLog = append(d.lastLog[:0], payload.Data...)
+	return true
+}
