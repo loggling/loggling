237f3e036197f41dd0e29194c997a86c8d1df2b6
solgitae <solgitae@outlook.com>
2026-04-02 07:15:34 +0900

    feat: introduce rotatable writer hook for SIGHUP

---
+++ pkg/engine/rotator.go
@@ -0,0 +1,68 @@
+package engine
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+type RotatableWriter struct {
+	mu       sync.RWMutex
+	filepath string
+	file     *os.File
+}
+
+func NewRotatableWriter(filePath string) (*RotatableWriter, error) {
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return nil, err
+	}
+
+	rw := &RotatableWriter{
+		filepath: filePath,
+	}
+
+	if err := rw.Rotate(); err != nil {
+		return nil, err
+	}
+
+	return rw, nil
+}
+
+func (rw *RotatableWriter) Write(p []byte) (n int, err error) {
+	rw.mu.RLock()
+	defer rw.mu.RUnlock()
+	return rw.file.Write(p)
+}
+
+func (rw *RotatableWriter) Rotate() error {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+
+	if rw.file != nil {
+		rw.file.Close()
+	}
+
+	f, err := os.OpenFile(rw.filepath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Printf("[Rotator] Failed to open log file: %v", err)
+		return err
+	}
+
+	rw.file = f
+	log.Printf("[Rotator] Log file successfully rotated: %s", rw.filepath)
+
+	return nil
+}
+
+func (rw *RotatableWriter) Close() error {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+
+	if rw.file != nil {
+		return rw.file.Close()
+	}
+
+	return nil
+}
